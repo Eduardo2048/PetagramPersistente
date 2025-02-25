16b0c8209edca63878107b25a990a2ffaa587773
Eduardo2048 <eduardo.bertucci@gmail.com>
2025-02-25 19:43:27 -0300

    Agregado el menu y la actividad acerca_de-

---
+++ Petagram2/app/src/main/java/com/eab/petagram2/AcercaDe.java
@@ -0,0 +1,39 @@
+package com.eab.petagram2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AcercaDe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_acerca_de);
+
+        Toolbar miToolbar=(Toolbar) findViewById(R.id.miActionBarBio);
+        setSupportActionBar(miToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+
+
+        setSupportActionBar(miToolbar);
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ Petagram2/app/src/main/java/com/eab/petagram2/MasBuscados.java
@@ -0,0 +1,71 @@
+package com.eab.petagram2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+import java.util.ArrayList;
+
+public class MasBuscados extends AppCompatActivity   {
+    private String TAG= "DBG_EAB";
+    ArrayList<Mascota> mascotas;
+    RecyclerView ListaMascotas;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.mas_buscados);
+       Toolbar miToolbar=(Toolbar) findViewById(R.id.miActionBarMB);
+        setSupportActionBar(miToolbar);
+        getSupportActionBar().setIcon(R.drawable.ic_pata);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+
+
+        setSupportActionBar(miToolbar);
+
+
+        ListaMascotas=(RecyclerView) findViewById(R.id.rvMascotasMB);
+        // GridLayoutManager   LLM= new GridLayoutManager(this,2);
+        LinearLayoutManager LLM= new LinearLayoutManager(this);
+        LLM.setOrientation(LinearLayoutManager.VERTICAL);
+        ListaMascotas.setLayoutManager(LLM);
+        InicializarListaMascotas();
+        InicializarAdaptador();
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mas_buscados), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+
+    private void InicializarAdaptador() {
+        MascotaAdapter Adapter = new MascotaAdapter(mascotas,this);
+        ListaMascotas.setAdapter(Adapter);
+    }
+    private void  InicializarListaMascotas()
+    {
+        mascotas=new ArrayList<Mascota>();
+        mascotas.add(new Mascota(R.drawable.perro5,getResources().getString(R.string.perro5),6,R.drawable.ic_hueso1));
+        mascotas.add(new Mascota(R.drawable.perro8,getResources().getString(R.string.perro8),5,R.drawable.ic_hueso1));
+        mascotas.add(new Mascota(R.drawable.perro4,getResources().getString(R.string.perro4),4,R.drawable.ic_hueso1));
+        mascotas.add(new Mascota(R.drawable.perro1,getResources().getString(R.string.perro1),3,R.drawable.ic_hueso1));
+        mascotas.add(new Mascota(R.drawable.perro7,getResources().getString(R.string.perro7),2,R.drawable.ic_hueso1));
+
+
+    }
+}
