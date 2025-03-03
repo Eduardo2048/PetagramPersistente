ef6ab2c00dd314bbd594e6b05b1834927eceecc4
Eduardo2048 <eduardo.bertucci@gmail.com>
2025-03-03 10:51:02 -0300

    Agregado el menu y la actividad acerca_de-

--- Petagram2/app/src/main/java/com/eab/petagram2/Contacto.java
+++ Petagram2/app/src/main/java/com/eab/petagram2/Contacto.java
@@ -1,6 +1,11 @@
 package com.eab.petagram2;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Contacto extends AppCompatActivity {
+    private Button SendMail;
+    private TextView tv_Email;
+    private TextView tv_Contacto;
+    private TextView tv_Menasaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +34,35 @@ public class Contacto extends AppCompatActivity {
 
         setSupportActionBar(miToolbar);
 
+        SendMail=(Button)findViewById(R.id.bt_mail);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+@SuppressLint("ResourceType")
+public void CambiaACorreo(View v)
+{
+    tv_Email=(TextView)findViewById(R.id.cnt_email);
+    tv_Contacto=(TextView)findViewById(R.id.cnt_nombre);
+    tv_Menasaje=(TextView)findViewById(R.id.cnt_menaje);
+
+    String miNombre = String.valueOf(tv_Contacto.getText());
+    String miMail=String.valueOf(tv_Email.getText());
+    String miMensaje = String.valueOf(tv_Menasaje.getText());
+
+    Intent intent = new Intent(v.getContext(),Correo.class);
+
+    intent.putExtra(getResources().getString(R.string.clave_email),miMail);
+    intent.putExtra(getResources().getString(R.string.clave_mensaje),miMensaje);
+    intent.putExtra(getResources().getString(R.string.clave_nomebre),miNombre);
+
+    startActivity(intent);
 }
+
+
+}
+
