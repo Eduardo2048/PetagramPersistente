c20613f3c85ee225dc47dbff39aa086656bb4865
Eduardo2048 <eduardo.bertucci@gmail.com>
2025-03-15 20:36:25 -0300

    Commit inicial Agregado Layouts

---
+++ PetagramPersistente/app/src/main/java/com/eab/petagrampersistente/Contacto.java
@@ -0,0 +1,80 @@
+package com.eab.petagram2;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Contacto extends AppCompatActivity {
+    private Button SendMail;
+    private TextView tv_Email;
+    private TextView tv_Contacto;
+    private TextView tv_Menasaje;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contacto);
+        Toolbar miToolbar=(Toolbar) findViewById(R.id.miActionBarCnt);
+        setSupportActionBar(miToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+
+
+        setSupportActionBar(miToolbar);
+
+        SendMail=(Button)findViewById(R.id.bt_mail);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+@SuppressLint("ResourceType")
+public void CambiaACorreo(View v)
+{
+    tv_Email=(TextView)findViewById(R.id.cnt_email);
+    tv_Contacto=(TextView)findViewById(R.id.cnt_nombre);
+    tv_Menasaje=(TextView)findViewById(R.id.cnt_menaje);
+
+    String miNombre = String.valueOf(tv_Contacto.getText());
+    String miMail=String.valueOf(tv_Email.getText());
+    String miMensaje = String.valueOf(tv_Menasaje.getText());
+
+    if (miNombre.isBlank()) {
+        Toast.makeText(getApplicationContext(), getResources().getString(R.string.error_nombre), Toast.LENGTH_LONG).show();
+    }
+    else if (miMail.isBlank()) {
+        Toast.makeText(getApplicationContext(), getResources().getString(R.string.error_mail), Toast.LENGTH_LONG).show();
+    }
+    else if (miMensaje.isBlank()) {
+        Toast.makeText(getApplicationContext(), getResources().getString(R.string.error_mensaje), Toast.LENGTH_LONG).show();
+    }
+    else {
+        Intent intent = new Intent(v.getContext(), Correo.class);
+
+        intent.putExtra(getResources().getString(R.string.clave_email), miMail);
+        intent.putExtra(getResources().getString(R.string.clave_mensaje), miMensaje);
+        intent.putExtra(getResources().getString(R.string.clave_nomebre), miNombre);
+
+        startActivity(intent);
+    }
+}
+
+
+}
+
---
+++ PetagramPersistente/app/src/main/java/com/eab/petagrampersistente/Mascota.java
@@ -0,0 +1,48 @@
+package com.eab.petagram2;
+
+public class Mascota {
+private int i_Imagen;
+private String s_Name;
+private int i_Likes;
+
+private int i_Hueso;
+
+    public Mascota(int i_Imagen, String s_Name, int i_Likes, int i_Hueso) {
+        this.i_Imagen = i_Imagen;
+        this.s_Name = s_Name;
+        this.i_Likes = i_Likes;
+        this.i_Hueso = i_Hueso;
+    }
+
+    public int getI_Hueso() {
+        return i_Hueso;
+    }
+
+    public void setI_Hueso(int i_Hueso) {
+        this.i_Hueso = i_Hueso;
+    }
+
+    public int getI_Imagen() {
+        return i_Imagen;
+    }
+
+    public void setI_Imagen(int i_Imagen) {
+        this.i_Imagen = i_Imagen;
+    }
+
+    public int getI_Likes() {
+        return i_Likes;
+    }
+
+    public void setI_Likes(int i_Likes) {
+        this.i_Likes = i_Likes;
+    }
+
+    public String getS_Name() {
+        return s_Name;
+    }
+
+    public void setS_Name(String s_Name) {
+        this.s_Name = s_Name;
+    }
+}
